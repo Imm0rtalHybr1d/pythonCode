6ee9ad75d1eed7d5095fa314db743b2a805d3145
Hano <hanomartin82@gmail.com>
2024-05-15 15:20:26 +0200

    lists again

--- Collections/Lists.py
+++ Collections/Lists.py
@@ -1,15 +1,17 @@
 #simple syntax to create a list 
-students = ['carlos','hano','nueeb','hiemie']
-
-my_list: list = [1, True, [1,2,3,4]] #- lists can hold any datatype as an element 
-
 #using square brackets to index into the list and get the second value of the list 
 #lists are zero indexed , meaning its first value is position/index 0
 #using the square brackets allows us to go inside a listand specify the value we want using an index
+
+students: list[str] = ['carlos','hano','nueeb','hiemie']
+
+my_list: list = [1, True, [1,2,3,4]] #- lists can hold any datatype as an element 
+
+
 print(students[1])
 
-for each_student in students:
-    print(each_student)
+for student in students:
+    print(student)
 
 
 #another way of doing this 
