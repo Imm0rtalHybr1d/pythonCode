d27be6aa326f6ef97f19d2b3ad76f8e10b845b70
Hano <hanomartin82@gmail.com>
2024-05-20 16:15:21 +0200

    args to tupple , kwargs to dict

--- Functions/args_kwargs.py
+++ Functions/args_kwargs.py
@@ -1,16 +1,25 @@
+# *ARGS_____________WILL ALWAYS CONVERT TO TUPPLE__________________________________
 # *args - refers to the arguments , **kwargs refers to keyword arguments
 
 #this function takes in args of type int and returns int
-# def add(*args:int) -> int: 
-#     print(args)
-#     return sum(args)
+def add(*args:int) -> int: 
+    print(args)
+    return sum(args)
 
-# print(add(1,2,3))
+print(add(1,2,3))
 
 #creates a function that takes in *args called people,
 # a positional arg called greeting 
 #and another arg called ending 
 def greet(*people:str,greeting:str,ending:str) -> None:
-    for person in people:
+    #passing multiple values into *args creates a tupple by default
+    #we then use a for loop to iterate over through the tupple
+    for person in people: 
         print(f'{greeting} {person}, {ending}')
 greet('Hano','Hiemie','Nueeb',greeting='Hello',ending='we gyming today? ') 
+
+# *KWARGS________________________________________________
+def pin_position(**kwargs: int) -> None:
+    print(kwargs)
+    
+pin_position()    
