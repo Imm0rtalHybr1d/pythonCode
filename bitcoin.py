9b6c8be1d687768ffe167c1ea7a4eaedc9415560
Hano <hanomartin82@gmail.com>
2024-06-06 15:27:29 +0200

    BTC Project

--- bitcoin.py
+++ bitcoin.py
@@ -1,16 +1,25 @@
 import sys
 import requests
 
+
+print('This program is a simple Bitcoin price checker >>>')
+currency:str = input('Please choose a currency:')
+print('1 - USD')
+print('2 - EUR')
+print('3 - GDP')
+print('4 - ZAR')
 try:
     sys.argv[1]  = float(sys.argv[1])    
     #we can also ensure that user has entered the right amount of args using if len(arg[])    
     if len(sys.argv) == 2:
-        #gets the Json file from the API         
+        #gets the JSON file from the API         
         response =  requests.get(f'https://api.coindesk.com/v1/bpi/currentprice.json')
 
-        #stores the Json file data in this variable    
+        #stores the JSON file data in this variable    
         o = response.json()
+        
         try:
+            
             #converts rate to a float by removing the comma
             rate = float(o['bpi']['USD']['rate'].replace(",", ""))
             print(f'Current price of BTC: ${rate:,.4f}')
