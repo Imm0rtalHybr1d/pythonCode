fbf2d89c2043f3aae4ca57cf301a7a620399b7f5
Hano <hanomartin82@gmail.com>
2024-07-01 10:36:53 +0200

    m

---
+++ calc.py
@@ -0,0 +1,14 @@
+from os import system
+
+system('cls')
+
+def main()-> None:
+    x:int = int(input('What is X? '))
+    print(f'x squared is {square(x)}')
+    
+def square(x:int) -> int:
+   return x*x     
+ 
+if __name__ == "__main__":
+    main()
+ 
